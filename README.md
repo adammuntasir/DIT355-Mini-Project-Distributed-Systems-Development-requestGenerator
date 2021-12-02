0736b806f3920f2e9d104160fe6ca442369bb99d
mehrvarz <mehrvarz@student.chalmers.se>
2021-12-02 17:09:22 +0100

    project setup and readme file

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # RequestGenerator
 
+### Purpose
+
+The functionality of request Generator component in Dentistimo project is that it stimulates the generation of booking requests.
+
+### Technologies used
+
+- MQTT with HiveMQ broker
+- Node.js
+
+### Dependencies
+
+1. Node.js & Npm (https://nodejs.org/en/download/)
+2. Git (https://git-scm.com/downloads)
+
+### Setup
+
+1. Move the file Global_values in the Documentations folder to the parent directory
+2. Move the registry file from the documentation to the parent
+3. To set up the request Generator repository we use "npm i mqtt" command
+
+### Running
+
+To run this node we use "node index" command
+
+### Link to the main Team-8 documentation ReadMe file
+
+https://git.chalmers.se/courses/dit355/test-teams-formation/team-8/team-8-project/-/blob/main/README.md
---
+++ src/publisher.js
@@ -0,0 +1,42 @@
+//Reference: https://www.npmjs.com/package/mqtt
+
+//Setup of publisher
+
+var mqtt = require("mqtt");
+var options = { qos: 1, keepalive: 0, reconnectPeriod: 5000 };
+var access = require("../../global_values");
+ip = access.ip_address;
+port = access.tcp_port;
+options = access.options;
+var client = mqtt.connect("tcp://" + ip + ":" + port, options);
+
+var publisher = {
+  start: function () {
+    //Called when client is connected
+    client.on("connect", function () {
+      console.log("Status: Publisher is connected to broker");
+    });
+
+    //Called when client is disconnected
+    client.on("disconnect", function () {
+      console.log("Status: Publisher has been disconnected");
+    });
+
+    //Called when client is reconnecting
+    client.on("reconnect", function () {
+      console.log("Status: Publisher is reconnecting");
+    });
+
+    //Called when client is offline
+    client.on("offline", function () {
+      console.log("Status: Publisher is offline");
+      client.reconnect();
+    });
+  },
+
+  publish: function publishing(topic, message) {
+    client.publish(topic, message);
+  },
+};
+
+module.exports = publisher;
