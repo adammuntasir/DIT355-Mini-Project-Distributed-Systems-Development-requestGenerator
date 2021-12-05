03c8e0f56c257eeda8d4a5b290e915fb2fe79901
mehrvarz <mehrvarz@student.chalmers.se>
2021-12-05 16:20:00 +0100

    index.js file update
    Co-authored-by: Muntasir Adam  <adammu@student.chalmers.se>

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 var publisher = require("./src/publisher.js");
-//var reqGenerator = require("./src/generator.js");
+var reqGenerator = require("./src/generator.js");
+let myGen = new reqGenerator();
 
 var access = require("../global_values.js");
 
 publisher.start(); //starts the publisher.js module
-
+publisher.publish(access.FromClient, '[("2021", "12", "10", "10", "00")]');
 publisher.publish(
   access.FromClient,
-  {
+  JSON.stringify({
     userId: 178279,
     requestId: 37,
     dentistID: 2,
     issuance: 1638369312826,
     date: "2021-12-10 10:00",
     clientCoordinates: [11.942625, 57.685255],
-  }[("2021", "12", "10", "10", "00")]
+  })
 );
 
-console.log("message sent");
+console.log(myGen.generate(1));
+
+
