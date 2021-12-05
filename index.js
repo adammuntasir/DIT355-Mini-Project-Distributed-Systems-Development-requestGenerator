f7502219ada643f0cea640d66c2c0463dfa39761
sramzi <sramzi@student.chalmers.se>
2021-12-05 08:41:03 +0100

    latest one

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 var publisher = require("./src/publisher.js");
-//var reqGenerator = require("./src/generator.js");
+var Generator = require('./src/generator.js');
 
-var access = require("../global_values.js");
+let generate = new Generator();
+
+var reqId = 0;
+
+//Set pump settings
+
+
 
 publisher.start(); //starts the publisher.js module
-publisher.publish(access.FromClient, '[("2021", "12", "10", "10", "00")]');
-publisher.publish(
-  access.FromClient,
-  JSON.stringify({
-    userId: 178279,
-    requestId: 37,
-    dentistID: 2,
-    issuance: 1638369312826,
-    date: "2021-12-10 10:00",
-    clientCoordinates: [11.942625, 57.685255],
-  })
-);
 
-console.log("message sent");
+setInterval(function() {
+
+    var generatedMsg = generate.generate(reqId);
+    reqId++;
+    try {
+        console.log(generatedMsg)
+        publisher.publish(generatedMsg);
+
+    } catch (error) {
+        console.log(error.message)
+
+    }
+}, 500);
--- src/generator.js
+++ src/generator.js
@@ -1,29 +1,43 @@
-let dateGenerator = require("random-date-generator");
-
 class generator {
-  constructor() {}
-  generate(requestId) {
-    var emptyObj = {
-      userId: "",
-      requestId: "",
-      dentistID: "",
-      issuance: "",
-      date: "",
-      clientCoordinates: "",
-    };
-    var idFilled = this.idGenerate(emptyObj, requestId);
-    var dentistIdFilled = this.getDentistId(idFilled);
-    var issuanceFilled = this.issuanceGenerate(dentistIdFilled);
-    var dateFilled = this.generateDate(issuanceFilled);
-    var clientCoordinatesFilled = this.generateCoordinates(dateFilled);
-    var completedStringified = JSON.stringify(issuanceFilled);
-    return completedStringified;
-  }
+    constructor() {}
+    generate(requestId) {
 
-  //Methods:
-  idGenerate(emptyObj, requestId) {}
-  getDentistId(idFilled) {}
-  issuanceGenerate(dentistIdFilled) {}
-  generateDate(issuanceFilled) {}
-  generateCoordinates(dateFilled) {}
+        var emptyObj = {
+            userId: "",
+            requestId: "",
+            dentistID: "",
+            issuance: "",
+            date: "",
+            clientCoordinates: ""
+        };
+
+        var idFilled = this.idGenerate(emptyObj, requestId);
+        var issuanceFilled = this.issuanceGenerate(idFilled)
+        var completedStringified = JSON.stringify(issuanceFilled);
+        return completedStringified;
+    }
+
+
+    idGenerate(emptyVar, requestId) {
+
+        emptyVar.userId = parseInt(Math.random() * (500000 - 1) + 1); // already random
+
+        emptyVar.requestId = requestId + parseInt(Math.random() * ((requestId * 153) / 2 - 1) + 1);; // already random
+
+        emptyVar.dentistID = 3; // make random
+
+        emptyVar.date = 2022 + "-" + 6 + "-" + 31 + " " + 10 + ":" + 30; // make random
+
+        emptyVar.clientCoordinates = [22.942625, 33.685255] // keep this 
+        return emptyVar;
+
+    }
+
+    issuanceGenerate(emptyVar) {
+        var currentTime = Date.now();
+        emptyVar.issuance = currentTime; // create issuance variable for client object
+        return emptyVar;
+    }
 }
+
+module.exports = generator;
