62e3b43b2882b7575237b73a276a3f3b3d882c34
sramzi <sramzi@student.chalmers.se>
2021-12-06 15:46:14 +0100

    changed the frequency

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var publisher = require("./src/publisher.js");
 var Generator = require('./src/generator.js');
-
+var access = require('../global_values.js')
 let generate = new Generator();
 
 var reqId = 0;
@@ -17,10 +17,10 @@ setInterval(function() {
     reqId++;
     try {
         console.log(generatedMsg)
-        publisher.publish(generatedMsg);
+        publisher.publish(access.sendToCircuitBreaker, generatedMsg);
 
     } catch (error) {
         console.log(error.message)
 
     }
-}, 500);
+}, 50);
