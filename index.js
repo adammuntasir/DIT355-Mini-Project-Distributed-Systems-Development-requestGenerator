92a7836a8354f7c46e79f31a1546eb884e1dd2dc
Muntasir Adam <gusadammu@student.gu.se>
2021-12-06 11:23:29 +0100

    fix location and date generator
    Co-authored-by: Mila Mehrvarz <mehrvarz@student.chalmers.se>"

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 var publisher = require("./src/publisher.js");
 var reqGenerator = require("./src/generator.js");
-let myGen = new reqGenerator();
+let engine = new reqGenerator();
 
 var access = require("../global_values.js");
 
 publisher.start(); //starts the publisher.js module
-publisher.publish(access.FromClient, '[("2021", "12", "10", "10", "00")]');
-publisher.publish(
-  access.FromClient,
-  JSON.stringify({
-    userId: 178279,
-    requestId: 37,
-    dentistID: 2,
-    issuance: 1638369312826,
-    date: "2021-12-10 10:00",
-    clientCoordinates: [11.942625, 57.685255],
-  })
-);
 
-console.log(myGen.generate(1));
+var reqId = 0;
 
-
+setInterval(function () {
+  publisher.publish(access.sendToCircuitBreaker, engine.generate(1));
+  console.log(engine.generate(1));
+}, 1000);
--- src/generator.js
+++ src/generator.js
@@ -3,23 +3,6 @@ let emptyVar;
 
 class generator {
   constructor() {}
-  generate(requestId) {
-    var emptyObj = {
-      userId: "",
-      requestId: "",
-      dentistID: "",
-      issuance: "",
-      date: "",
-      clientCoordinates: "",
-    };
-    var idFilled = this.idGenerate(emptyObj, requestId);
-    var dentistIdFilled = this.getDentistId(idFilled);
-    var issuanceFilled = this.issuanceGenerate(dentistIdFilled);
-    var dateFilled = this.generateDate(issuanceFilled);
-    var clientCoordinatesFilled = this.generateCoordinates(dateFilled);
-    var completedStringified = JSON.stringify(issuanceFilled);
-    return completedStringified;
-  }
 
   //Methods:
   idGenerate(emptyObj, requestId) {
@@ -27,8 +10,24 @@ class generator {
     emptyObj.requestId = requestId;
     return emptyObj;
   }
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+
+  getFixedMinute(minute) {
+    var fixedMinute = minute;
+    if (fixedMinute >= 45 || fixedMinute <= 15) {
+      fixedMinute = 0;
+    } else {
+      fixedMinute = 30;
+    }
+    return fixedMinute;
+  }
+
   getDentistId(idFilled) {
-    idFilled.dentistID = getRandomInt(1, 6);
+    idFilled.dentistID = this.getRandomInt(1, 5);
     return idFilled;
   }
   issuanceGenerate(dentistIdFilled) {
@@ -36,7 +35,7 @@ class generator {
     return dentistIdFilled;
   }
   generateDate(issuanceFilled) {
-    var startDate = new Date(2021, 0, 1);
+    /* var startDate = new Date(2021, 0, 1);
     var endDate = new Date(2022, 11, 31);
     var dateAndTime = dateGenerator.getRandomDateInRange(startDate, endDate);
     var time = "";
@@ -58,18 +57,32 @@ class generator {
       minute
     );
     issuanceFilled.date = newTime;
+    */
+    issuanceFilled.date = "2021-12-10 10:00";
     return issuanceFilled;
   }
   generateCoordinates(dateFilled) {
     var latitude = Math.random() * (58.077909 - 57.302823) + 57.302823;
     var longitude = Math.random() * (12.984824 - 11.424765) + 11.424765;
-    dateFilled.clientCoordinates = [latitude, longitude];
+    // dateFilled.clientCoordinates = [latitude, longitude];
+    dateFilled.clientCoordinates = [22.942625, 33.685255];
   }
-
-  getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  generate(requestId) {
+    var emptyObj = {
+      userId: "",
+      requestId: "",
+      dentistID: "",
+      issuance: "",
+      date: "",
+      clientCoordinates: "",
+    };
+    var idFilled = this.idGenerate(emptyObj, requestId);
+    var dentistIdFilled = this.getDentistId(idFilled);
+    var issuanceFilled = this.issuanceGenerate(dentistIdFilled);
+    var dateFilled = this.generateDate(issuanceFilled);
+    var clientCoordinatesFilled = this.generateCoordinates(dateFilled);
+    var completedStringified = JSON.stringify(issuanceFilled);
+    return completedStringified;
   }
 }
 
